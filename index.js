7528661b72f53cc10f2be512c935f862491ac98f
HarryCandlish <hcnd90@gmail.com>
2019-06-17 15:16:59 +1200

    some examples of comparisons

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 module.exports = {
-  isEqual
+  isEqual,
+  isStrictlyEqual,
+  isNotEqual,
+  greaterThan
 };
 
-//  ( == ) operator converts the operands if they are not the same type
+// Comparisons
+
+//  ( == ) Equal: operator converts the operands if they are not the same type
 //  then applies comparisons.
 
 function isEqual(a, b) {
   return b == a;
+}
+
+// ( === ) Strict Equal : operator compares the operands, and will return true if the operands
+// are equal and are the same data type.
+
+function isStrictlyEqual(a, b) {
+  x = Number(a);
+  return x === b;
+}
+
+// ( !== ) Strict not Equal : operator returns true if the
+// operands are not equal and not the same data type.
+
+function isNotEqual(a, b) {
+  x = toString(a);
+  return x !== b;
+}
+
+//  ( > ) Greater Than : returns true if the left operand is greater than
+//  the right.
+
+function greaterThan(a) {
+  var x = a.filter(n => n > 5);
+  return x;
 }
--- tests.test.js
+++ tests.test.js
@@ -7,3 +7,26 @@ test("return true so as a and b are equal", () => {
   const actual = testOne.isEqual(a, b);
   expect(actual).toEqual(expected);
 });
+
+test("return true using the strict equal operator", () => {
+  const a = "1";
+  const b = 1;
+  const expected = true;
+  const actual = testOne.isStrictlyEqual(a, b);
+  expect(actual).toEqual(expected);
+});
+
+test("convert a to string  to not equal and return false", () => {
+  const a = 1;
+  const b = 1;
+  const expected = true;
+  const actual = testOne.isNotEqual(a, b);
+  expect(actual).toEqual(expected);
+});
+
+test("return the numbers higher than 5", () => {
+  const a = [1, 2, 3, 4, 5, 6, 7, 8];
+  const expected = [6, 7, 8];
+  const actual = testOne.greaterThan(a);
+  expect(actual).toEqual(expected);
+});
