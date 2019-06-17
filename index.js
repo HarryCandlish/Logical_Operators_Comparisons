491a28f5e912ed8a6b5208ccdc566b6a7efff29d
HarryCandlish <hcnd90@gmail.com>
2019-06-17 14:31:06 +1200

    == demonstrated and test written

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  isEqual
+};
+
+//  ( == ) operator converts the operands if they are not the same type
+//  then applies comparisons.
+
+function isEqual(a, b) {
+  return b == a;
+}
--- tests.test.js
+++ tests.test.js
@@ -1,1 +1,9 @@
 const testOne = require("./index");
+
+test("return true so as a and b are equal", () => {
+  const a = "1";
+  const b = 1;
+  const expected = true;
+  const actual = testOne.isEqual(a, b);
+  expect(actual).toEqual(expected);
+});
